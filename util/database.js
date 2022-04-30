aedac5c39e55fb10fd82b2299f17fe1f3fb674fe
ndera-bt <85622453+ndera-bt@users.noreply.github.com>
2022-04-30 22:07:22 +0100

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("istory", "root", "0987654321", {
+const sequelize = new Sequelize("db_name", "root", "db_password", {
   dialect: "mysql",
   host: "localhost",
 });
